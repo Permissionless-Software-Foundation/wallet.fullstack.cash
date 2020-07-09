255bf8198b92e9fd5100e7c505cc0b331fa0531b
Chris Troutner <chris.troutner@gmail.com>
2020-07-08 18:31:22 -0700

    fix(customized): Customized for wallet.fullstack.cash

---
+++ src/about/index.js
@@ -0,0 +1,33 @@
+/*
+  About
+*/
+
+import React from 'react'
+import { Row, Col, Content, Box, Button } from 'adminlte-2-react'
+
+class About extends React.Component {
+  render () {
+    return (
+      <Content
+        title='About'
+        subTitle=''
+        browserTitle='About | wallet.fullstack.cash'
+      >
+        <Row>
+          <Col xs={12}>
+            <Box
+              title='wallet.fullstack.cash'
+              type='primary'
+              closable
+              collapsable
+            >
+              This is the official web wallet provided by FullStack.cash.
+            </Box>
+          </Col>
+        </Row>
+      </Content>
+    )
+  }
+}
+
+export default About
