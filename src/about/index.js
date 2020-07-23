c9b7e38b7031f4b1691701f4a3b09e06273edd9e
Chris Troutner <chris.troutner@gmail.com>
2020-07-23 13:33:00 -0700

    fix(about): Updating about page

--- src/about/index.js
+++ src/about/index.js
@@ -9,19 +9,32 @@ class About extends React.Component {
   render () {
     return (
       <Content
-        title='About'
-        subTitle=''
-        browserTitle='About | wallet.fullstack.cash'
+        title="About"
+        subTitle=""
+        browserTitle="About | wallet.fullstack.cash"
       >
         <Row>
           <Col xs={12}>
             <Box
-              title='wallet.fullstack.cash'
-              type='primary'
+              title="wallet.fullstack.cash"
+              type="primary"
               closable
               collapsable
             >
-              This is the official web wallet provided by FullStack.cash. More data soon.
+              <p>
+                This is the official web wallet provided by FullStack.cash. You
+                can access this web wallet over Tor and IPFS. See the links in
+                the footer at the bottom of the page.
+              </p>
+              <p>
+                This wallet app is currently in an 'Open Alpha' test. The
+                general public is invited to use it, but be aware that the app
+                is in-progress, the code is in-flux, and many things will change
+                or break. Please solicit feedback on this app in our{' '}
+                <a href="https://t.me/permissionless_software" target="_blank">
+                  Telegram Channel
+                </a>
+              </p>
             </Box>
           </Col>
         </Row>
