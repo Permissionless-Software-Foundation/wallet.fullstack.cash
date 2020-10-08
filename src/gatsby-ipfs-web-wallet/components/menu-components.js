ea5be53017f9c94360c2fbc4326037ffe19db7e6
Chris Troutner <chris.troutner@gmail.com>
2020-10-07 21:46:20 -0700

    fix(menu): Redering default menu items

--- src/gatsby-ipfs-web-wallet/components/menu-components.js
+++ src/gatsby-ipfs-web-wallet/components/menu-components.js
@@ -8,23 +8,56 @@
 
 import React from 'react'
 import { Sidebar } from 'adminlte-2-react'
+
+import Wallet from 'gatsby-ipfs-web-wallet/src/components/admin-lte/wallet'
+import Tokens from 'gatsby-ipfs-web-wallet/src/components/admin-lte/tokens'
+import Configure from 'gatsby-ipfs-web-wallet/src/components/admin-lte/configure'
+import SendReceive from 'gatsby-ipfs-web-wallet/src/components/admin-lte/send-receive'
+
 import About from '../../about'
 import Sweep from 'gatsby-plugin-bch-sweep/src/components/sweep'
 
 const { Item } = Sidebar
 
-const MenuComponents = [
-  {
-    key: 'Sweep',
-    // component: <><p>test</p></>,
-    component: <Sweep key='Sweep' />,
-    menuItem: <Item icon='fas-cog' key='Sweep' text='Sweep' />
-  },
-  {
-    key: 'About',
-    component: <About />,
-    menuItem: <Item icon='fas-cog' key='About' text='About' />
-  }
-]
+const MenuComponents = props => {
+  return [
+    {
+      key: 'Tokens',
+      component: <Tokens key="Tokens" {...props} />,
+      menuItem: <Item icon="fas-coins" key="Tokens" text="Tokens" />
+    },
+    {
+      key: 'Send/Receive BCH',
+      component: <SendReceive key="Send/Receive BCH" {...props} />,
+      menuItem: (
+        <Item
+          icon="fa-exchange-alt"
+          key="Send/Receive BCH"
+          text="Send/Receive BCH"
+        />
+      )
+    },
+    {
+      key: 'Sweep',
+      component: <Sweep key="Sweep" {...props} />,
+      menuItem: <Item icon="fas-cog" key="Sweep" text="Sweep" />
+    },
+    {
+      key: 'Wallet',
+      component: <Wallet key="Wallet" {...props} />,
+      menuItem: <Item icon="fa-wallet" key="Wallet" text="Wallet" />
+    },
+    {
+      key: 'Configure',
+      component: <Configure key="Configure" {...props} />,
+      menuItem: <Item icon="fas-cog" key="Configure" text="Configure" />
+    },
+    {
+      key: 'About',
+      component: <About key="About" {...props} />,
+      menuItem: <Item icon="fas-cog" key="About" text="About" />
+    }
+  ]
+}
 
 export default MenuComponents
