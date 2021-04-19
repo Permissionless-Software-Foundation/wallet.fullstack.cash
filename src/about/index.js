8fc9243d068606baa85598426f74388e15699137
Chris Troutner <chris.troutner@gmail.com>
2021-04-19 14:35:15 -0700

    More verbage on the About View

--- src/about/index.js
+++ src/about/index.js
@@ -9,24 +9,24 @@ class About extends React.Component {
   render () {
     return (
       <Content
-        title='About'
-        subTitle=''
-        browserTitle='About | wallet.fullstack.cash'
+        title="About"
+        subTitle=""
+        browserTitle="About | wallet.fullstack.cash"
       >
         <Row>
           <Col xs={12}>
             <Box
-              title='wallet.fullstack.cash'
-              type='primary'
+              title="wallet.fullstack.cash"
+              type="primary"
               closable
               collapsable
             >
               <p>
                 This is the official web wallet provided by{' '}
                 <a
-                  href='https://fullstack.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://fullstack.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   FullStack.cash
                 </a>
@@ -41,17 +41,17 @@ class About extends React.Component {
                 Learn more about the code and infrastructure powering this
                 wallet at{' '}
                 <a
-                  href='https://fullstack.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://fullstack.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   FullStack.cash
                 </a>{' '}
                 Please solicit feedback on this app in our{' '}
                 <a
-                  href='https://t.me/bch_js_toolkit'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://t.me/bch_js_toolkit"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Telegram Channel
                 </a>
@@ -60,39 +60,40 @@ class About extends React.Component {
               <p>
                 The code for this web wallet can be found{' '}
                 <a
-                  href='https://github.com/Permissionless-Software-Foundation/wallet.fullstack.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/Permissionless-Software-Foundation/wallet.fullstack.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   on GitHub
                 </a>
                 . It's based on the{' '}
                 <a
-                  href='https://github.com/Permissionless-Software-Foundation/bch-wallet-starter'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/Permissionless-Software-Foundation/bch-wallet-starter"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   bch-wallet-starter Gatsby Starter
                 </a>
                 , the{' '}
                 <a
-                  href='https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/Permissionless-Software-Foundation/gatsby-ipfs-web-wallet"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   gatsby-ipfs-web-wallet Gatsby Theme
                 </a>
                 , and the{' '}
                 <a
-                  href='https://github.com/Permissionless-Software-Foundation/minimal-slp-wallet'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://github.com/Permissionless-Software-Foundation/minimal-slp-wallet"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   minimal-slp-wallet
                 </a>{' '}
                 wallet engine. We encourage other businesses to copy and modify
                 the wallet code for their own use.
               </p>
+
               <p>
                 This wallet has full support for Bitcoin Cash (BCH) and eCash
                 (BCHA) blockchains. That includes SLP tokens, including
@@ -102,13 +103,38 @@ class About extends React.Component {
                 It helps support the developers who work on this web wallet and
                 other software sponsored by the{' '}
                 <a
-                  href='https://psfoundation.cash'
-                  target='_blank'
-                  rel='noopener noreferrer'
+                  href="https://psfoundation.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
                   Permissionless Software Foundation
                 </a>
                 .
+              </p>
+
+              <p>
+                This app can be compiled into a native Android or iOS app using{' '}
+                <a
+                  href="https://react-freelancer.ch/blog/build-mobile-apps-using-gatsby-and-capacitor"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Capacitor
+                </a>
+                . The Android APK for this app can be{' '}
+                <a
+                  href="https://github.com/Permissionless-Software-Foundation/wallet.fullstack.cash"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  downloaded from the GitHub repository
+                </a>
+                . But, the Android app will run into rate limits unless you add
+                a JWT token from FullStack.cash into the Configuration View. We
+                don't actively support use of this web app as a phone app, but
+                instead provide the APK as proof
+                that other businesses can fork this wallet app to kick-off
+                development of their own phone app.
               </p>
             </Box>
           </Col>
