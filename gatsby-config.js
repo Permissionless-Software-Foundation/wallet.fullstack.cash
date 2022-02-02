3e26f5f52a5c7258e0b681a0ea2f997af4c72ce2
Chris Troutner <chris.troutner@gmail.com>
2022-02-02 11:53:07 -0800

    fix(minimal-slp-wallet): updating to v4.1.0

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,7 +7,7 @@
 module.exports = {
   /* Your site config here */
   plugins: [
-    'gatsby-theme-bch-wallet',
+    "fullstack-gatsby-theme-bch-wallet",
     // 'bch-wallet-plugin-postoffice',
     // {
     //   resolve: 'gatsby-plugin-compile-es6-packages',
@@ -16,9 +16,8 @@ module.exports = {
     //   }
     // },
     // require.resolve('/home/trout/work/psf/gatsby-plugin-bch-sweep')
-    'gatsby-plugin-bch-sweep',
-    'gatsby-plugin-bch-create-token'
-
+    "gatsby-plugin-bch-sweep",
+    "gatsby-plugin-bch-create-token"
   ],
   // https://www.gatsbyjs.com/docs/reference/release-notes/v2.28/#feature-flags-in-gatsby-configjs
   flags: {
