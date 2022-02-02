7ab50ffe8c8f4a8ae620afc273d8b170d7791204
Chris Troutner <chris.troutner@gmail.com>
2022-02-02 11:53:22 -0800

    linting

--- src/gatsby-theme-bch-wallet/components/menu-components.js
+++ src/gatsby-theme-bch-wallet/components/menu-components.js
@@ -6,65 +6,65 @@
   It over-rides he default file in the gatsby-ipfs-web-wallet Theme.
 */
 
-import React from "react"
-import { Sidebar } from "adminlte-2-react"
+import React from 'react'
+import { Sidebar } from 'adminlte-2-react'
 
-import Wallet from "fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/wallet"
-import Tokens from "fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/tokens"
-import Configure from "fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/configure"
-import SendReceive from "fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/send-receive"
+import Wallet from 'fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/wallet'
+import Tokens from 'fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/tokens'
+import Configure from 'fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/configure'
+import SendReceive from 'fullstack-gatsby-theme-bch-wallet/src/components/admin-lte/send-receive'
 
-import About from "../../about"
-import Sweep from "gatsby-plugin-bch-sweep/src/components/sweep/index"
-import CreateToken from "gatsby-plugin-bch-create-token/src/components/create-token"
+import About from '../../about'
+import Sweep from 'gatsby-plugin-bch-sweep/src/components/sweep/index'
+import CreateToken from 'gatsby-plugin-bch-create-token/src/components/create-token'
 
 const { Item } = Sidebar
 
 const MenuComponents = props => {
   return [
     {
-      key: "Tokens",
-      component: <Tokens key="Tokens" {...props} />,
-      menuItem: <Item icon="fas-coins" key="Tokens" text="Tokens" />
+      key: 'Tokens',
+      component: <Tokens key='Tokens' {...props} />,
+      menuItem: <Item icon='fas-coins' key='Tokens' text='Tokens' />
     },
     {
-      key: "Send/Receive BCH",
-      component: <SendReceive key="Send/Receive BCH" {...props} />,
+      key: 'Send/Receive BCH',
+      component: <SendReceive key='Send/Receive BCH' {...props} />,
       menuItem: (
         <Item
-          icon="fa-exchange-alt"
-          key="Send/Receive BCH"
-          text="Send/Receive BCH"
+          icon='fa-exchange-alt'
+          key='Send/Receive BCH'
+          text='Send/Receive BCH'
         />
       )
     },
     {
-      key: "Sweep",
-      component: <Sweep key="Sweep" {...props} />,
-      menuItem: <Item icon="fas-arrow-circle-up" key="Sweep" text="Sweep" />
+      key: 'Sweep',
+      component: <Sweep key='Sweep' {...props} />,
+      menuItem: <Item icon='fas-arrow-circle-up' key='Sweep' text='Sweep' />
     },
     {
-      key: "Wallet",
-      component: <Wallet key="Wallet" interface="rest-api" {...props} />,
+      key: 'Wallet',
+      component: <Wallet key='Wallet' interface='rest-api' {...props} />,
       // component: <Wallet key="Wallet" interface="consumer-api" {...props} />,
-      menuItem: <Item icon="fa-wallet" key="Wallet" text="Wallet" />
+      menuItem: <Item icon='fa-wallet' key='Wallet' text='Wallet' />
     },
     {
-      key: "Create Token",
-      component: <CreateToken key="Create Token" {...props} />,
+      key: 'Create Token',
+      component: <CreateToken key='Create Token' {...props} />,
       menuItem: (
-        <Item icon="fas-plus-square" key="Create Token" text="Create Token" />
+        <Item icon='fas-plus-square' key='Create Token' text='Create Token' />
       )
     },
     {
-      key: "Configure",
-      component: <Configure key="Configure" {...props} />,
-      menuItem: <Item icon="fas-cog" key="Configure" text="Configure" />
+      key: 'Configure',
+      component: <Configure key='Configure' {...props} />,
+      menuItem: <Item icon='fas-cog' key='Configure' text='Configure' />
     },
     {
-      key: "About",
-      component: <About key="About" {...props} />,
-      menuItem: <Item icon="fas-question" key="About" text="About" />
+      key: 'About',
+      component: <About key='About' {...props} />,
+      menuItem: <Item icon='fas-question' key='About' text='About' />
     }
   ]
 }
